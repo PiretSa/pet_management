23bbc7a9c80261b160bb91aa22420eacb2059011
PiretSa <92926766+PiretSa@users.noreply.github.com>
2023-05-07 10:40:59 +0300

    CRUD Backend

--- src/main/java/com/pets/controller/CrudRestController.java
+++ src/main/java/com/pets/controller/CrudRestController.java
@@ -1,19 +1,44 @@
 package com.pets.controller;
 
-import org.springframework.web.bind.annotation.RestController;
-import com.pets.model.Pet;
-
 import java.util.ArrayList;
 import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import com.pets.model.Pet;
+import com.pets.services.CrudService;
+
 
 @RestController
 public class CrudRestController {
+    @Autowired
+    private CrudService service;
+
+    @GetMapping("/getpetlist")
     public List<Pet> fetchPetList() {
+
         List<Pet> pets = new ArrayList<Pet>();
 
         // Logic to fetch list from database
-
+        pets = service.fetchPetList();
 
         return pets;
+    }
+
+    @PostMapping("/addpet")
+    public Pet savePet(@RequestBody Pet pet) {
+
+        return service.savePetToDB(pet);
+    }
+
+    @GetMapping("/getpetbyid/{id}")
+    public Pet fetchPetById(@PathVariable int id) {
+
+        return service.fetchPetById(id).get();
+    }
+
+    @DeleteMapping("/deletepetbyid/{id}")
+    public String deletePetById(@PathVariable int id) {
+
+        return service.deletePetById(id);
     }
 }
--- src/main/java/com/pets/services/CrudService.java
+++ src/main/java/com/pets/services/CrudService.java
@@ -1,7 +1,40 @@
 package com.pets.services;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.pets.model.Pet;
+import com.pets.repo.CrudRepo;
 
 @Service
 public class CrudService {
+
+    @Autowired
+    private CrudRepo repo;
+
+    public List<Pet> fetchPetList() {
+        return repo.findAll();
+    }
+
+    public Pet savePetToDB(Pet pet) {
+        return repo.save(pet);
+    }
+
+    public Optional<Pet> fetchPetById(int id) {
+        return repo.findById(id);
+    }
+
+    public String deletePetById(int id) {
+        String result;
+        try {
+            repo.deleteById(id);
+            result = "Pet successfully deleted";
+        } catch (Exception e) {
+            result = "Pet is not deleted";
+        }
+        return result;
+    }
 }
